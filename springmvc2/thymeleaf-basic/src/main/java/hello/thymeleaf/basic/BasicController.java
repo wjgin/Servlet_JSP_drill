57b9a25396fed4d9d6d4534666f1052a58eafe61
wjgin <woojin0913@naver.com>
2022-08-11 22:34:09 +0900

    feat: add thymeleaf 다양한 데이터 접근 방법과 지역변수 사용 방법 templates/basic/text-variable.html 추가 Controller textVariable 함수 추가

--- springmvc2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ springmvc2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,15 +1,12 @@
 package hello.thymeleaf.basic;
 
 import lombok.Data;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import java.time.LocalDateTime;
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -30,4 +27,36 @@ public class BasicController {
         model.addAttribute("data", "Hello <b>Spring!<b>");
         return "basic/text-escaped";
     }
+
+    @GetMapping("variable")
+    public String variableTest(Model model) {
+        User userA = new User("userA", 10);
+        User userB = new User("userB", 20);
+
+        ArrayList<User> users = new ArrayList<>();
+        users.add(userA);
+        users.add(userB);
+
+        Map<String, User> userMap = new HashMap<String, User>();
+        userMap.put("userA", userA);
+        userMap.put("userB", userB);
+
+        model.addAttribute("userA", userA);
+        model.addAttribute("users", users);
+        model.addAttribute("userMap", userMap);
+
+        return "basic/text-variable";
+    }
+
+    @Data
+    static class User {
+        private String name;
+        private int age;
+
+        public User(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+    }
+
 }
